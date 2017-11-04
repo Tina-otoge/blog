3986e3fd4e5f083eb84dec3018c45417a889c27b
Damien Savatier <damien.savatier@epitech.eu>
2017-11-04 21:59:48 +0100

    Made _site ignore rule absolute

---
+++ new_post.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+d=$(date -I)
+
+f='_posts/'$d-$1.markdown
+
+d+=' '$(date +"%T %z")
+
+echo '---'           >> $f
+echo 'layout: post'  >> $f
+echo "title: \"$1\"" >> $f
+echo "date: $d"      >> $f
+echo 'categories:'   >> $f
+echo 'author: Tina'  >> $f
+echo 'tags:'         >> $f
+echo ''              >> $f
+echo '---'           >> $f
+
+if [ $EDITOR != '' ]; then
+    $EDITOR $f
+fi
